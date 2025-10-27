25950ac9bf869b58fb99a997e2c16f081be3e3a7
Samuel-Pinheiro-C-Lopes <samuelpinheiroclopes@gmail.com>
2025-10-27 20:01:24 -0300

    Application entry point added

---
+++ src/main/java/io/github/samuel_pinheiro_c_lopes/itau_challenge_99/ItauChallenge99Application.java
@@ -0,0 +1,13 @@
+package io.github.samuel_pinheiro_c_lopes.itau_challenge_99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauChallenge99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauChallenge99Application.class, args);
+	}
+
+}
