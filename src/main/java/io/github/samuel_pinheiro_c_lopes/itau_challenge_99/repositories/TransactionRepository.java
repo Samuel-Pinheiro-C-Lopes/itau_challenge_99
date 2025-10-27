780907c3ca3caa88ca6a302d9f6b56f5d527da1f
Samuel-Pinheiro-C-Lopes <samuelpinheiroclopes@gmail.com>
2025-10-27 19:28:53 -0300

    Transactions repository created

---
+++ src/main/java/io/github/samuel_pinheiro_c_lopes/itau_challenge_99/repositories/TransactionRepository.java
@@ -0,0 +1,33 @@
+package io.github.samuel_pinheiro_c_lopes.itau_challenge_99.repositories;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.models.Transaction;
+
+@Service
+public class TransactionRepository {
+	private final List<Transaction> transactions;
+	
+	public TransactionRepository() {
+		this.transactions = new ArrayList<Transaction>();
+	}
+	
+	public List<Transaction> findAll() {
+		return this.transactions;
+	}
+	
+	public List<Transaction> findByDateTimeGreaterThan(final OffsetDateTime dateTime) {
+		return this.transactions
+				.stream()
+				.filter(t -> t.dateTime().isAfter(dateTime))
+				.toList();
+	}
+	
+	public Boolean save(final Transaction transaction) {
+		return this.transactions.add(transaction);
+	}
+}
