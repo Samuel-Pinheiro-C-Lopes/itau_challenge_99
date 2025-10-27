efff59cc981dfbc4091d30ca4e591448c184ca17
Samuel-Pinheiro-C-Lopes <samuelpinheiroclopes@gmail.com>
2025-10-27 20:00:53 -0300

    added to dtos utility factory method and constructor

--- src/main/java/io/github/samuel_pinheiro_c_lopes/itau_challenge_99/dtos/TransactionStatisticsResponseDTO.java
+++ src/main/java/io/github/samuel_pinheiro_c_lopes/itau_challenge_99/dtos/TransactionStatisticsResponseDTO.java
@@ -2,14 +2,14 @@ package io.github.samuel_pinheiro_c_lopes.itau_challenge_99.dtos;
 
 import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.models.TransactionStatistics;
 
-public record TransactionStatisticsDTO(
+public record TransactionStatisticsResponseDTO(
 		Integer count, 
 		Long sum, 
 		Long max, 
 		Long min, 
 		Long avg
 ) {
-	public TransactionStatisticsDTO(final TransactionStatistics statistics) {
+	public TransactionStatisticsResponseDTO(final TransactionStatistics statistics) {
 		this(
 				statistics.getCount(), 
 				statistics.getSum(), 
