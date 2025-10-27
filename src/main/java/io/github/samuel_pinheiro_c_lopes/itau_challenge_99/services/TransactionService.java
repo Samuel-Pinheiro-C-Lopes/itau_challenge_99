6dfe7b16c93e7d41588229cc763e242e109d1aac
Samuel-Pinheiro-C-Lopes <samuelpinheiroclopes@gmail.com>
2025-10-27 19:58:26 -0300

    segregated services and delegated statistics retrieval to the TransactionsStatisticsService

--- src/main/java/io/github/samuel_pinheiro_c_lopes/itau_challenge_99/services/TransactionService.java
+++ src/main/java/io/github/samuel_pinheiro_c_lopes/itau_challenge_99/services/TransactionService.java
@@ -1,14 +1,9 @@
 package io.github.samuel_pinheiro_c_lopes.itau_challenge_99.services;
 
-import java.time.OffsetDateTime;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.dtos.TransactionStatisticsDTO;
-import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.models.Transaction;
-import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.models.TransactionStatistics;
+import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.dtos.TransactionRequestDTO;
 import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.repositories.TransactionRepository;
 
 @Service
@@ -22,12 +17,11 @@ public class TransactionService {
 		this.transactionStatisticsService = transactionStatisticsService;
 	}
 	
-	public TransactionStatisticsDTO getStatistics(final OffsetDateTime offset) {
-		final List<Transaction> transactions = this.transactionRepository.findByDateTimeGreaterThan(offset);
-		final TransactionStatistics statistics = new TransactionStatistics();
-		
-		transactions.forEach(t ->transactionStatisticsService.updateStatistics(statistics, t));
-		
-		return new TransactionStatisticsDTO(statistics);
+	public Boolean save(final TransactionRequestDTO transactionRequest) {
+		return this.transactionRepository.save(transactionRequest.toTransaction());
+	}
+	
+	public void deleteAll() {
+		this.transactionRepository.deleteAll();
 	}
 }
--- src/main/java/io/github/samuel_pinheiro_c_lopes/itau_challenge_99/services/TransactionStatisticsService.java
+++ src/main/java/io/github/samuel_pinheiro_c_lopes/itau_challenge_99/services/TransactionStatisticsService.java
@@ -1,19 +1,51 @@
 package io.github.samuel_pinheiro_c_lopes.itau_challenge_99.services;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.dtos.TransactionStatisticsResponseDTO;
 import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.models.Transaction;
 import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.models.TransactionStatistics;
+import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.repositories.TransactionRepository;
 
 @Service
 public class TransactionStatisticsService {
+	private final TransactionRepository transactionRepository;
 	
-	public void updateStatistics(final TransactionStatistics statistics, final Transaction newTransaction) {
+	@Autowired
+	public TransactionStatisticsService(final TransactionRepository transactionRepository) {
+		this.transactionRepository = transactionRepository;
+	}
+	
+	public TransactionStatisticsResponseDTO getStatistics(final OffsetDateTime offset) {
+		final List<Transaction> transactions = this.transactionRepository.findByDateTimeGreaterThan(offset);
+		final TransactionStatistics statistics = new TransactionStatistics();
+		this.updateStatistics(statistics, transactions);
+		return new TransactionStatisticsResponseDTO(statistics);
+	}
+	
+	
+	private void updateStatistics(final TransactionStatistics statistics, final List<Transaction> newTransactions) {
+		newTransactions.forEach(t -> this.updateStatisticsExceptAvg(statistics, t));
+		this.setNewAvg(statistics);
+	}
+	
+	private void updateStatistics(final TransactionStatistics statistics, final Transaction newTransaction) {
 		this.increaseCount(statistics);
 		this.increaseSum(statistics, newTransaction.value());
 		this.setNewMax(statistics, newTransaction.value());
 		this.setNewMin(statistics, newTransaction.value());
 		this.setNewAvg(statistics);
+	}
+	
+	private void updateStatisticsExceptAvg(final TransactionStatistics statistics, final Transaction newTransaction) {
+		this.increaseCount(statistics);
+		this.increaseSum(statistics, newTransaction.value());
+		this.setNewMax(statistics, newTransaction.value());
+		this.setNewMin(statistics, newTransaction.value());
 	}
 	
 	private void setNewAvg(final TransactionStatistics statistics) {
