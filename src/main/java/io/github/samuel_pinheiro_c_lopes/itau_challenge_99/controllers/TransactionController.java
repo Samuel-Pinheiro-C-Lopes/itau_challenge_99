6aab667c1279e12bd4e35cc98b70c58d47e8d36f
Samuel-Pinheiro-C-Lopes <samuelpinheiroclopes@gmail.com>
2025-10-27 19:31:19 -0300

    Created empty transaction controller

---
+++ src/main/java/io/github/samuel_pinheiro_c_lopes/itau_challenge_99/controllers/TransactionController.java
@@ -0,0 +1,8 @@
+package io.github.samuel_pinheiro_c_lopes.itau_challenge_99.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransactionController { }
