4155f50210f0ef26437d73d563a9ac307041e5a9
Samuel-Pinheiro-C-Lopes <samuelpinheiroclopes@gmail.com>
2025-10-28 00:16:05 -0300

    Log service created

---
+++ src/main/java/io/github/samuel_pinheiro_c_lopes/itau_challenge_99/log/services/TransactionLogService.java
@@ -0,0 +1,35 @@
+package io.github.samuel_pinheiro_c_lopes.itau_challenge_99.log.services;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.log.dtos.TransactionLogResponseDTO;
+import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.log.enums.OperationType;
+import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.log.models.TransactionLog;
+import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.log.repositories.TransactionLogRepository;
+import io.github.samuel_pinheiro_c_lopes.itau_challenge_99.transaction.models.Transaction;
+
+@Service
+public class TransactionLogService {
+	private final TransactionLogRepository transactionLogRepository;
+	
+	@Autowired
+	public TransactionLogService(final TransactionLogRepository transactionLogRepository) {
+		this.transactionLogRepository = transactionLogRepository;
+	}
+	
+	public void save(final Transaction transaction, final OperationType operationType) {
+		this.transactionLogRepository.save(new TransactionLog(transaction, OffsetDateTime.now(), operationType));
+	}
+	
+	public List<TransactionLogResponseDTO> findAll() {
+		return this.transactionLogRepository.findAll().stream().map(TransactionLogResponseDTO::new).toList();
+	}
+	
+	public void deleteAll() {
+		this.transactionLogRepository.deleteAll();
+	}
+}
